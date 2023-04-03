d0ecb1aa8ee8f5cf7d696a75f44f4d5f1ea71f94
stop1204 <hl928452957@gmail.com>
2023-04-03 16:09:09 +0800

    session no log , just change trace to debug

--- src/session.rs
+++ src/session.rs
@@ -4,6 +4,6 @@ use log::trace;
 pub fn trace_msg(s: String) {
     // chinese charactor
     if !s.contains("invalid utf-8 sequence") {
-        trace!("{}", s);
+        log::debug!("{}", s);
     }
 }
