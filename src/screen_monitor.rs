ebfc47434cf377e214d22abc06bcd3ec80ee63f3
stop1204 <hl928452957@gmail.com>
2024-06-16 01:49:12 +0800

    add ffmpeg.exe

---
+++ src/screen_monitor.rs
@@ -0,0 +1,131 @@
+use std::fmt::Error;
+use std::fs;
+use std::process::{Command, exit, Stdio};
+use log::{info, error, trace, debug, warn};
+use std::fs::{create_dir_all, remove_dir_all};
+use std::path::Path;
+use chrono::{Local, Duration as ChronoDuration, NaiveDate};
+use log4rs;
+use crate::structs::ConfigEnv;
+
+
+fn check_ffmpeg(ffmpeg_path: &str)->Result<(),Error> {
+    if Command::new(ffmpeg_path)
+        .arg("-version")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .is_err() {
+        error!("ffmpeg not found. Please install ffmpeg and make sure it is in your PATH.");
+        // exit(1); // this will cause the program to exit
+        return Err(Error::default());
+    }
+    Ok(())
+}
+
+fn create_directory(path: &str) {
+    if !Path::new(path).exists() {
+        info!("Creating directory: {}", path);
+        create_dir_all(path).expect("Failed to create directory");
+    }
+}
+
+fn remove_old_directories(base_path: &str) {
+    let seven_days_ago = Local::now() - ChronoDuration::days(7);
+    /*let old_date_path = format!("{}/{}", base_path, seven_days_ago.format("%Y-%m-%d").to_string());
+    if Path::new(&old_date_path).exists() {
+        info!("Removing old recordings directory: {}", old_date_path);
+        remove_dir_all(&old_date_path).expect("Failed to remove old recordings directory");
+    }*/
+    let cutoff_date = seven_days_ago.date().naive_local();
+
+    if let Ok(entries) = fs::read_dir(base_path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(entry_name) = entry.file_name().into_string() {
+                    if let Ok(entry_date) = NaiveDate::parse_from_str(&entry_name, "%Y-%m-%d") {
+                        if entry_date < cutoff_date {
+                            let old_date_path = format!("{}/{}", base_path, entry_name);
+                            warn!("Removing old recordings directory: {}", old_date_path);
+                            if let Err(e) = remove_dir_all(&old_date_path) {
+                                error!("Failed to remove old recordings directory {}: {}", old_date_path, e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        error!("Failed to read base directory: {}", base_path);
+    }
+}
+
+fn record_screen(ffmpeg_path: &str, file_path: &str, frame_rate: &u64, encoder: &String, preset: &String, crf: &u64, pix_fmt: &String, duration: &u64) {
+    let mut ffmpeg_command = Command::new(ffmpeg_path)
+        .args(&[
+            "-f", "gdigrab", // 使用 gdigrab 进行屏幕捕获
+            // "-framerate", "30", // 帧率
+            // "-i", "desktop", // 输入设备
+            // "-c:v", "libx264", // 使用 libx264 编码
+            // "-preset", "ultrafast", // 编码速度
+            // "-crf", "40", // 编码质量
+            // "-pix_fmt", "yuv420p", // 像素格式
+            // "-t", "5", // 录制时长（秒）
+            "-framerate", frame_rate.to_string().as_str(), // 帧率
+            "-i", "desktop", // 输入设备
+            "-c:v", encoder, // 使用 libx264 编码
+            "-preset", preset, // 编码速度
+            "-crf", crf.to_string().as_str(), // 编码质量
+            "-pix_fmt", pix_fmt, // 像素格式
+            "-t", duration.to_string().as_str(), // 录制时长（秒）
+            file_path, // 输出文件
+        ])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Failed to start ffmpeg process");
+    trace!("Recording screen to: {}", file_path);
+    ffmpeg_command.wait().expect("Failed to wait on ffmpeg process");
+}
+
+pub fn init() {
+    let cfg = ConfigEnv::from_env().expect("Failed to initialize project configuration");
+    if !cfg.monitor_screen.default { return; }
+    info!("screen_monitor init");
+
+
+
+    // let base_path = "./log/screen";
+    // let ffmpeg_path = "./ffmpeg.exe";
+    let base_path = &cfg.monitor_screen.base_path;
+    let ffmpeg_path = &cfg.monitor_screen.ffmpeg_path;
+    let frame_rate = &cfg.monitor_screen.frame_rate;
+    let encoder = &cfg.monitor_screen.encoder;
+    let preset = &cfg.monitor_screen.preset;
+    let crf = &cfg.monitor_screen.crf;
+    let pix_fmt = &cfg.monitor_screen.pix_fmt;
+    let duration = &cfg.monitor_screen.duration;
+
+
+    // 检查 ffmpeg 是否可用
+    if let Ok(_) = check_ffmpeg(ffmpeg_path) {} else {
+        return;
+    }
+
+    loop {
+        // 获取当前日期文件夹和时间戳
+        let current_date = Local::now().format("%Y-%m-%d").to_string();
+        let timestamp = Local::now().format("%H-%M-%S").to_string();
+        let date_path = format!("{}/{}", base_path, current_date);
+        let file_path = format!("{}/{}.mp4", date_path, timestamp);
+
+        // 创建当前日期的文件夹
+        create_directory(&date_path);
+
+        // 删除7天前的文件夹
+        remove_old_directories(base_path);
+
+        // 录制屏幕
+        record_screen(ffmpeg_path, &file_path, frame_rate, encoder, preset, crf, pix_fmt, duration);
+    }
+}
